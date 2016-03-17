4adc65e771f1ed1b61d94bcc02ef758846edc765
Juppi Roni-Otto TTK14S <roni-ottojuppi@kamk.fi>
2016-03-17 21:58:40 +0200

    merge and cannon edits

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,4 +6,10 @@ public class GameController : MonoBehaviour
     public int currentArea = 0;
     public int wave = 1;
     public int currentCannon = 0;
+
+    void Awake()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
 }
