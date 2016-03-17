e03a2cdc7cc326d770cacb8040f2399b48383f83
Juppi Roni-Otto TTK14S <roni-ottojuppi@kamk.fi>
2016-03-17 23:09:06 +0200

    mergeehä ny

---
+++ Assets/Scripts/Russian.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Russian : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        col.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        col.gameObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
