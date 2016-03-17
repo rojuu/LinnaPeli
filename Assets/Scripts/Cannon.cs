27216e9b1aa4fd89df0f41fa88867ffcb42ab0fb
Juppi Roni-Otto TTK14S <roni-ottojuppi@kamk.fi>
2016-03-17 15:59:21 +0200

    shooting

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class Cannon : MonoBehaviour {
 
-    public GameObject turret;
+    public GameObject barrel;
+    public GameObject cannonBall;
+    public GameObject shootLocation;
 
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0)  * Time.deltaTime  * 100);
-        turret.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), 0, 0) * Time.deltaTime * 100);
+        barrel.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), 0, 0) * Time.deltaTime * 100);
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject ammo = (GameObject)Instantiate(cannonBall, shootLocation.transform.position, shootLocation.transform.rotation);
+            ammo.GetComponent<Rigidbody>().AddForce(shootLocation.transform.forward * 1000);
+        }
 	}
 }
