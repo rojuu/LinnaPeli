48f6ea5f1fddf72ab3b6c59699d2b3d748f2c072
Juppi Roni-Otto TTK14S <roni-ottojuppi@kamk.fi>
2016-03-17 18:42:59 +0200

    crosshair too hard. skip

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+    public Texture2D cursorImage;
+    private int cursorWidth = 32;
+    private int cursorHeight = 32;
+    private Transform myTransform;
+    private Camera myCamera;
+
+    void Start()
+    {
+        myCamera = (Camera)GameObject.FindWithTag("MainCamera").GetComponent("Camera");
+        myTransform = transform;
+        Cursor.visible = true;
+
+    }
+
+    void OnGUI()
+    {
+        Vector3 screenPos = myCamera.WorldToScreenPoint(myTransform.position);
+        screenPos.y = Screen.height - screenPos.y; //The y coordinate on screenPos is inverted so we need to set it straight
+        GUI.DrawTexture(new Rect(screenPos.x, screenPos.y, cursorWidth, cursorHeight), cursorImage);
+    }
+}
