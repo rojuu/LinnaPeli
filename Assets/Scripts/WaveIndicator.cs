83c650a4c8b22bbc34562ef2cc9641a298af2e7c
Juppi Roni-Otto TTK14S <roni-ottojuppi@kamk.fi>
2016-03-17 19:14:59 +0200

    indicate

---
+++ Assets/Scripts/WaveIndicator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveIndicator : MonoBehaviour {
+
+    public bool indicate = false;
+
+    public float delay = 0.3f;
+    float time;
+
+	// Use this for initialization
+	void Start () {
+        time = delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time += Time.deltaTime;
+
+        if (time > delay && indicate)
+        {
+            time = 0f;
+            if (gameObject.GetComponent<Image>().color == Color.red)
+            {
+                gameObject.GetComponent<Image>().color = Color.white;
+
+            }
+            else
+            {
+                gameObject.GetComponent<Image>().color = Color.red;
+            }
+        }
+	    
+	}
+}
