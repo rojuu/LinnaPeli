6c549bc890401c54f78101b7a7369818a9fca64f
Juppi Roni-Otto TTK14S <roni-ottojuppi@kamk.fi>
2016-03-17 15:42:25 +0200

    turret turns

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+
+    public GameObject turret;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0)  * Time.deltaTime  * 100);
+        turret.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), 0, 0) * Time.deltaTime * 100);
+	}
+}
